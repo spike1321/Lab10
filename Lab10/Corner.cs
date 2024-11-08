37c2f438ee27da1dc7c1827c7b2db930055416ad
Павел Каплин <spike1321@yandex.ru>
2024-11-09 01:37:45 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Corner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Corner
+    {
+        public double Gradus { get; set; }
+        double Min;
+        double Sec;
+       public int bug = 0;
+
+        public Corner(double gradus, double min, double sec)
+        {
+                Gradus = gradus;
+                Min_ = min;
+                Sec_ = sec;
+        }
+        public double Min_
+        {
+            set
+            {
+                if (value <= 60 && value >= 0)
+                {
+                    Min = value;
+                }
+                else
+                {
+                    bug = 1;
+                    Console.WriteLine("Диапазон возможных значений, угловых  минут : [от 0 до 60]");
+                }
+            }
+            get
+            {
+                return Min;
+            }
+        }
+        public double Sec_
+        {
+            set
+            {
+                if (value <= 60 && value >= 0)
+                {
+                    Sec = value;
+                }
+                else
+                {
+                    bug = 1;
+                    Console.WriteLine("Диапазон возможных значений, угловых  секунд : [от 0 до 60]");
+                }
+            }
+            get
+            {
+                return Sec;
+            }
+        }
+
+        public string ToRadians()
+        {
+            if (bug == 0)
+            {
+                double radian = (Gradus + (Min + (Sec / 60)) / 60) * Math.PI / 180;
+                return radian.ToString();
+            }
+            else
+            {
+                return "Введены некорректные значения.";
+            }
+           
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Corner corner;
+            try
+            {
+               // do
+               // {
+                    Console.Write("Введите градусы: ");
+                    double gradus = double.Parse(Console.ReadLine());
+                    Console.Write("Введите минуты: ");
+                    double min = double.Parse(Console.ReadLine());
+                    Console.Write("Введите секунды: ");
+                    double sec = double.Parse(Console.ReadLine());
+
+                    corner = new Corner(gradus, min, sec);
+               // }
+               // while (corner.bug >0);
+                  Console.WriteLine("Радианый угол равен: {0:0.00}", double.TryParse(corner.ToRadians(), out double q) ? q : throw new Exception(corner.ToRadians()));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
